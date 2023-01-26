39f9bd5b3ca5a4468bb1daf9f376b1d447ba7769
Aqissiaq <aqissiaq@gmail.com>
2023-01-26 19:08:13 +0100

    Reduction semantics and a much cleaner correctness proof (that is just bisimulation in disguise tbh)

--- ParallelTraces.v
+++ ParallelTraces.v
@@ -13,6 +13,7 @@ From Coq Require Import Program.Equality.   (* for `dependent induction` *)
 From Coq Require Import Logic.FunctionalExtensionality. (* for equality of substitutions *)
 From Coq Require Import Relations.
 From Coq Require Import Classes.RelationClasses.
+From Coq Require Import Sets.Ensembles.
 
 From SymEx Require Import Expr.
 Import BasicExpr.
@@ -28,9 +29,9 @@ Open Scope com_scope.
 Open Scope string_scope.
 Open Scope trace_scope.
 
-(** Symbolic semantics *)
-Definition Var : Type := string.
+Ltac splits := repeat (try split).
 
+(** Symbolic semantics *)
 Inductive STrace_step : Type :=
 | STAsgn (x:Var) (e:Aexpr)
 | STCond (b:Bexpr).
@@ -64,38 +65,148 @@ Definition SConfig : Type := Stmt * STrace.
 
 Reserved Notation " c '->s' c' " (at level 40).
 Inductive Sstep : relation SConfig  :=
-| SAsgn_step : forall x e s t,
-    (<{x := e ; s}>, t) ->s (s, t :: STAsgn x e)
+| SAsgn_step : forall x e t,
+    (<{x := e}>, t) ->s (<{skip}>, t :: STAsgn x e)
+| SSeq_step : forall s1 s2 s t1 t2,
+    (s1, t1) ->s (s2, t2) ->
+    (<{s1 ; s}>, t1) ->s (<{s2 ; s}>, t2)
+| SSeq_done : forall s t,
+    (<{skip ; s}>, t) ->s (s, t)
 | SParLeft_step : forall s s' s'' t t',
   (s, t) ->s (s', t') ->
   (<{s || s''}>, t) ->s (<{s' || s''}>, t')
 | SParRight_step : forall s s' s'' t t',
   (s, t) ->s (s', t') ->
   (<{s'' || s}>, t) ->s (<{s'' || s'}>, t')
-| SIfTrue_step : forall b s1 s2 s t,
-    (<{if b {s1}{s2} ; s}>, t) ->s (<{s1 ; s}>, t :: STCond b)
-| SIfFalse_step : forall b s1 s2 s t,
-    (<{if b {s1}{s2} ; s}>, t) ->s (<{s2 ; s}>, t :: STCond (BNot b))
-| SSkipLeft : forall s t,
-    (<{skip || s}>, t) ->s (s, t)
-| SSkipRight : forall s t,
-    (<{s || skip}>, t) ->s (s, t)
+| SParLeft_done : forall s t,
+    (<{ skip || s }>, t) ->s (s, t)
+| SParRight_done : forall s t,
+    (<{ s || skip }>, t) ->s (s, t)
+| SIfTrue_step : forall b s1 s2 t,
+    (<{if b {s1}{s2}}>, t) ->s (s1, t :: STCond b)
+| SIfFalse_step : forall b s1 s2 t,
+    (<{if b {s1}{s2}}>, t) ->s (s2, t :: STCond (BNot b))
 where " c '->s' c' " := (Sstep c c').
 
 Definition multi_Sstep := clos_refl_trans_n1 _ Sstep.
 Notation " c '->*' c' " := (multi_Sstep c c') (at level 40) : trace_scope.
 
 (** Properties *)
-Lemma Sstep_progress : forall s s' t t',
-    (s, t) ->s (s', t') ->
-    s <> s' \/ t <> t'.
+
+Lemma progress__S : forall s t, s = SSkip \/ exists s' t', (s, t) ->s (s', t').
 Proof.
-  intros. dependent induction H; subst;
-    try (left; apply SSeq_disjoint);
-    try (right; apply cons_neq').
-  destruct (IHSstep s0 s'0 t t'); try reflexivity.
-  (* get back to this*)
-Admitted.
+  induction s; intros.
+  - right. exists SSkip. eexists. constructor.
+  - right. edestruct IHs1.
+    + exists s2. eexists. rewrite H. constructor.
+    + destruct H as [s' [t' IH]].
+      exists <{s'; s2}>. exists t'. apply SSeq_step. apply IH.
+  - right. edestruct IHs1.
+    + exists s2. eexists. rewrite H. constructor.
+    + destruct H as [s' [t' IH]].
+      exists <{s'|| s2}>. exists t'. apply SParLeft_step. apply IH.
+  - right. exists s1. eexists. apply SIfTrue_step.
+  - left. reflexivity.
+Qed.
+
+(* not deterministic, maybe prove later*)
+
+(* characterizing stuff in terms of sets of traces *)
+Definition Concatenate (A B: Ensemble STrace) : Ensemble STrace :=
+  fun t => exists t1 t2, t = t1 ++ t2 /\ A t1 /\ B t2.
+
+Lemma Concatenate_empty_left : forall S, Concatenate (Singleton _ []) S = S.
+Proof.
+  intros. apply Extensionality_Ensembles. split; intros t H.
+  - destruct H as [t1 [t2 [Happ [Hempty HS]]]].
+    inversion Hempty; subst. rewrite app_nil_l. assumption.
+  - exists []. exists t. splits.
+    + rewrite app_nil_l. reflexivity.
+    + assumption.
+Qed.
+
+Inductive is_interleaving : STrace -> STrace -> STrace -> Prop :=
+| both_empty : is_interleaving [] [] []
+| left_empty : forall t, is_interleaving [] t t
+| right_empty : forall t, is_interleaving t [] t
+| left_step : forall t1 t2 t x,
+    is_interleaving t1 t2 t ->
+    is_interleaving (t1::x) t2 (t::x)
+| right_step : forall t1 t2 t x,
+    is_interleaving t1 t2 t ->
+    is_interleaving t1 (t2::x) (t::x)
+.
+
+Definition Interleave (A B: Ensemble STrace) : Ensemble STrace :=
+  fun t => exists t1 t2, is_interleaving t1 t2 t /\ A t1 /\ B t2.
+
+Definition traces__S (s:Stmt) : Ensemble STrace := fun t => (s, []) ->* (<{skip}>, t).
+
+Lemma skip_stuck : forall t, ~ (exists s t', (SSkip, t) ->s (s, t')).
+Proof. intros t H. destruct H as [s [t' comp]]. inversion comp. Qed.
+
+Theorem skip_traces_spec : Same_set _ (traces__S <{ skip }>) (Singleton _ []).
+Proof. split.
+       - intros t H. unfold In, traces__S in H.
+         (* transform to look at FIRST step of computation *)
+         apply clos_rtn1_rt in H. apply clos_rt_rt1n in H.
+         inversion H. apply In_singleton.
+         destruct y. destruct (skip_stuck []).
+         exists s. exists s0. assumption.
+       - intros t H. inversion H; subst. apply rtn1_refl.
+Qed.
+
+Theorem asgn_traces_spec : forall x e, Same_set _ (traces__S <{ x := e }>) (Singleton _ [STAsgn x e]).
+Proof.
+  split; intros t H.
+  - unfold In, traces__S in H.
+    apply clos_rtn1_rt in H. apply clos_rt_rt1n in H.
+    inversion H. inversion H0; subst.
+    assert (t_spec: t = [STAsgn x e])
+      by (dependent induction H1; [reflexivity| inversion H0]).
+    rewrite t_spec. apply In_singleton.
+  - inversion H. econstructor. apply SAsgn_step. constructor.
+Qed.
+
+Lemma step_traces_spec : forall s1 s2 t1 t2,
+    (s1, []) ->s (SSkip, t1) ->
+    (s2, t1) ->s (SSkip, t2) ->
+    (<{s1 ; s2}>, []) ->* (SSkip, t2).
+Proof.
+  intros.
+  apply clos_rt_rtn1. apply clos_rt1n_rt.
+  econstructor. apply SSeq_step. apply H.
+  econstructor. apply SSeq_done.
+  econstructor.  apply H0. constructor.
+Qed.
+
+Lemma seq_first : forall s1 s2 s t t',
+    (s1, t) ->* (s2, t') ->
+    (<{s1; s}>, t) ->* (<{s2;s}>, t').
+Proof.
+  intros. dependent induction H.
+  - constructor.
+  - destruct y. eapply Relation_Operators.rtn1_trans. apply SSeq_step. apply H.
+    apply IHclos_refl_trans_n1; reflexivity.
+Qed.
+
+Lemma seq_second : forall s1 s2 t t',
+    (s1, []) ->* (SSkip, t) ->
+    (s2, t) ->* (SSkip, t') ->
+    (<{s1; s2}>, []) ->* (SSkip, t').
+Proof.
+  intros. apply seq_first with (s := s2) in H.
+  (* juggling transitivity properties *)
+  apply clos_rt_rtn1. apply rt_trans with (y := (<{skip ; s2}>, t)).
+  apply clos_rtn1_rt. apply H.
+  apply clos_rt1n_rt. econstructor. apply SSeq_done.
+  apply clos_rt_rt1n. apply clos_rtn1_rt. apply H0.
+Qed.
+
+(** pipe dreams *)
+(* Theorem seq_traces_spec : forall s1 s2, Same_set _ (traces__S <{s1 ; s2}>) (Concatenate (traces__S s1) (traces__S s2)). *)
+(* Theorem par_traces_spec : forall s1 s2, Same_set _ (traces__S <{s1 || s2}>) (Interleave (traces__S s1) (traces__S s2)). *)
+
 
 (** Concrete Semantics *)
 
@@ -118,23 +229,28 @@ Definition Beval_t (V0:Valuation) (t:CTrace) (e:Bexpr) : bool :=
 Definition CConfig : Type := Stmt * CTrace.
 
 Inductive Cstep (V0:Valuation) : relation CConfig :=
-| CAsgn_step : forall x e t s,
-    Cstep V0 (<{x := e ; s}>, t) (s, t :: (x, Aeval_t V0 t e))
+| CAsgn_step : forall x e t,
+    Cstep V0 (<{x := e}>, t) (SSkip, t :: (x, Aeval_t V0 t e))
+| CSeq_step : forall s1 s2 s t1 t2,
+    Cstep V0 (s1, t1) (s2, t2) ->
+    Cstep V0 (<{s1;s}>, t1) (<{s2;s}>, t2)
+| CSeq_done : forall s t,
+    Cstep V0 (<{skip;s}>, t) (s, t)
 | CParLeft_step : forall s s' s'' t t',
     Cstep V0 (s, t) (s'', t') ->
     Cstep V0 (<{s || s'}>, t) (<{s'' || s'}>, t')
 | CParRight_step : forall s s' s'' t t',
     Cstep V0 (s, t) (s'', t') ->
     Cstep V0 (<{s' || s}>, t) (<{s' || s''}>, t')
-| CIfTrue_step : forall b s1 s2 s t,
+| CIfTrue_step : forall b s1 s2 t,
     Beval_t V0 t b = true ->
-    Cstep V0 (<{if b {s1}{s2} ; s}>, t) (<{s1 ; s}>, t)
-| CIfFalse_step : forall b s1 s2 s t,
+    Cstep V0 (<{if b {s1}{s2}}>, t) (s1, t)
+| CIfFalse_step : forall b s1 s2 t,
     Beval_t V0 t b = false ->
-    Cstep V0 (<{if b {s1}{s2} ; s}>, t) (<{s2 ; s}>, t)
-| CSkipLeft_step : forall s t,
+    Cstep V0 (<{if b {s1}{s2}}>, t) (s2, t)
+| CParLeft_done : forall s t,
     Cstep V0 (<{skip || s}>, t) (s, t)
-| CSkipRight_step : forall s t,
+| CParRight_done : forall s t,
     Cstep V0 (<{s || skip}>, t) (s, t).
 
 Definition multi_Cstep (V0:Valuation) := clos_refl_trans_n1 _ (Cstep V0).
@@ -149,69 +265,109 @@ Proof.
     try (simpl in H0; assumption).
   - eapply IHSstep. reflexivity. reflexivity. assumption.
   - eapply IHSstep. reflexivity. reflexivity. assumption.
+  - eapply IHSstep. reflexivity. reflexivity. assumption.
   - simpl in H0. apply andb_true_iff in H0. destruct H0. assumption.
   - simpl in H0. apply andb_true_iff in H0. destruct H0. assumption.
 Qed.
 
-Ltac splits := repeat (try split).
+(* Lemma par_step_correct : forall s s' t t' t0 V0, *)
+(*     (s, t) ->s (s', t') -> *)
+(*     Beval V0 (pc t') = true -> *)
+(*     acc_val V0 t0 = Comp V0 (acc_subst id_sub t) -> *)
+(*     forall s0, exists tc, *)
+(*       Cstep V0 (<{s || s0}>, t0) (<{s' || s0}>, tc) *)
+(*       /\ Cstep V0 (<{s0 || s}>, t0) (<{s0 || s'}>, tc) *)
+(*       /\ acc_val V0 tc = Comp V0 (acc_subst_id t'). *)
+(* Proof. *)
+(*   intros. dependent induction H. *)
+(*   - exists (t0 :: (x, Aeval_t V0 t0 e)). splits. *)
+(*     + apply CParLeft_step. apply CAsgn_step. *)
+(*     + apply CParRight_step. apply CAsgn_step. *)
+(*     + unfold acc_subst_id, Aeval_t. simpl. rewrite asgn_sound. rewrite H1. reflexivity. *)
+(*   - edestruct (IHSstep s1 s2 t t') as [tc [comp [_ trace]]]; *)
+(*       try reflexivity; try assumption. *)
+(*     exists tc. splits. *)
+(*     + apply CParLeft_step. apply comp. *)
+(*     + apply CParRight_step. apply comp. *)
+(*     + assumption. *)
+(*   - edestruct (IHSstep s0 s'0 t t') as [tc [_ [comp trace]]]; *)
+(*       try reflexivity; try assumption. *)
+(*     exists tc. splits. *)
+(*     + apply CParLeft_step. apply comp. *)
+(*     + apply CParRight_step. apply comp. *)
+(*     + assumption. *)
+(*   - exists t0. *)
+(*     (* rewrite path condition just once *) *)
+(*     simpl in H0. apply andb_true_iff in H0. destruct H0. *)
+(*     unfold Bapply_t in H. rewrite <- comp_subB in H. *)
+(*     unfold acc_subst_id in H. rewrite <- H1 in H. unfold Beval_t. *)
+(*     splits; *)
+(*       [ apply CParLeft_step; apply CIfTrue_step *)
+(*       | apply CParRight_step; apply CIfTrue_step *)
+(*       | unfold acc_subst_id; simpl]; *)
+(*       assumption. *)
+(*   - exists t0. *)
+(*     simpl in H0. apply andb_true_iff in H0. destruct H0. *)
+(*     apply negb_true_iff in H. *)
+(*     unfold Bapply_t in H. rewrite <- comp_subB in H. *)
+(*     unfold acc_subst_id in H. rewrite <- H1 in H. unfold Beval_t. *)
+(*     splits; *)
+(*       [ apply CParLeft_step; apply CIfFalse_step *)
+(*       | apply CParRight_step; apply CIfFalse_step *)
+(*       | unfold acc_subst_id; simpl]; *)
+(*       assumption. *)
+(*   - exists t0. splits. *)
+(*     + apply CParLeft_step. apply CSkipLeft_step. *)
+(*     + apply CParRight_step. apply CSkipLeft_step. *)
+(*     + assumption. *)
+(*   - exists t0. splits. *)
+(*     + apply CParLeft_step. apply CSkipRight_step. *)
+(*     + apply CParRight_step. apply CSkipRight_step. *)
+(*     + assumption. *)
+(* Qed. *)
 
-Lemma par_step_correct : forall s s' t t' t0 V0,
-    (s, t) ->s (s', t') ->
-    Beval V0 (pc t') = true ->
-    acc_val V0 t0 = Comp V0 (acc_subst id_sub t) ->
-    forall s0, exists tc,
-      Cstep V0 (<{s || s0}>, t0) (<{s' || s0}>, tc)
-      /\ Cstep V0 (<{s0 || s}>, t0) (<{s0 || s'}>, tc)
-      /\ acc_val V0 tc = Comp V0 (acc_subst_id t').
+Lemma pc_monotone_step : forall V0 s1 s2 t1 t2, (s1, t1) ->s (s2, t2) -> Beval V0 (pc t2) = true -> Beval V0 (pc t1) = true.
 Proof.
-  intros. dependent induction H.
-  - exists (t0 :: (x, Aeval_t V0 t0 e)). splits.
-    + apply CParLeft_step. apply CAsgn_step.
-    + apply CParRight_step. apply CAsgn_step.
-    + unfold acc_subst_id, Aeval_t. simpl. rewrite asgn_sound. rewrite H1. reflexivity.
-  - edestruct (IHSstep s0 s'0 t t') as [tc [comp [_ trace]]];
-      try reflexivity; try assumption.
-    exists tc. splits.
-    + apply CParLeft_step. apply comp.
-    + apply CParRight_step. apply comp.
-    + assumption.
-  - edestruct (IHSstep s0 s'0 t t') as [tc [_ [comp trace]]];
-      try reflexivity; try assumption.
-    exists tc. splits.
-    + apply CParLeft_step. apply comp.
-    + apply CParRight_step. apply comp.
-    + assumption.
-  - exists t0.
-    (* rewrite path condition just once *)
-    simpl in H0. apply andb_true_iff in H0. destruct H0.
-    unfold Bapply_t in H. rewrite <- comp_subB in H.
-    unfold acc_subst_id in H. rewrite <- H1 in H. unfold Beval_t.
-    splits;
-      [ apply CParLeft_step; apply CIfTrue_step
-      | apply CParRight_step; apply CIfTrue_step
-      | unfold acc_subst_id; simpl];
-      assumption.
-  - exists t0.
-    simpl in H0. apply andb_true_iff in H0. destruct H0.
-    apply negb_true_iff in H.
-    unfold Bapply_t in H. rewrite <- comp_subB in H.
-    unfold acc_subst_id in H. rewrite <- H1 in H. unfold Beval_t.
-    splits;
-      [ apply CParLeft_step; apply CIfFalse_step
-      | apply CParRight_step; apply CIfFalse_step
-      | unfold acc_subst_id; simpl];
-      assumption.
-  - exists t0. splits.
-    + apply CParLeft_step. apply CSkipLeft_step.
-    + apply CParRight_step. apply CSkipLeft_step.
-    + assumption.
-  - exists t0. splits.
-    + apply CParLeft_step. apply CSkipRight_step.
-    + apply CParRight_step. apply CSkipRight_step.
-    + assumption.
+  intros. dependent induction H; simpl in H0;
+    try (apply andb_true_iff in H0; destruct H0);
+    try assumption.
+  - eapply IHSstep. reflexivity. reflexivity. assumption.
+  - eapply IHSstep. reflexivity. reflexivity. assumption.
+  - eapply IHSstep. reflexivity. reflexivity. assumption.
 Qed.
 
 (** Correctness *)
+Theorem correctness_step : forall s s' t0 t t0' V0,
+    (s, t0) ->s (s', t) ->
+    Beval V0 (pc t) = true ->
+    acc_val V0 t0' = Comp V0 (acc_subst_id t0) ->
+    exists t', Cstep V0 (s, t0') (s', t') /\ acc_val V0 t' = Comp V0 (acc_subst_id t).
+Proof.
+  intros. dependent induction H;
+    try (eexists; splits; [constructor | assumption]).
+  - eexists. splits.
+    + apply CAsgn_step.
+    + unfold acc_subst_id, Aeval_t. simpl. rewrite asgn_sound.
+      rewrite H1. reflexivity.
+  - edestruct IHSstep as [t' [IHcomp IHabs]]; try reflexivity; try assumption.
+    eexists. splits.
+    + apply CSeq_step. apply IHcomp.
+    + assumption.
+  - edestruct IHSstep as [t' [IHcomp IHabs]]; try reflexivity; try assumption.
+    eexists. splits.
+    + apply CParLeft_step. apply IHcomp.
+    + assumption.
+  - edestruct IHSstep as [t' [IHcomp IHabs]]; try reflexivity; try assumption.
+    eexists. splits.
+    + apply CParRight_step. apply IHcomp.
+    + assumption.
+  - simpl in H0. apply andb_true_iff in H0. destruct H0. unfold Bapply_t in H.
+    rewrite <- comp_subB in H. unfold Beval_t. rewrite H1. assumption.
+  - simpl in H0. apply andb_true_iff in H0. destruct H0. apply negb_true_iff in H. unfold Bapply_t in H.
+    rewrite <- comp_subB in H. unfold Beval_t. rewrite H1. assumption.
+Qed.
+
+
 Theorem correctness : forall s s' t0 t t0' V0,
     (s, t0) ->* (s', t) ->
     Beval V0 (pc t) = true ->
@@ -219,65 +375,14 @@ Theorem correctness : forall s s' t0 t t0' V0,
     exists t', multi_Cstep V0 (s, t0') (s', t') /\ acc_val V0 t' = Comp V0 (acc_subst_id t).
 Proof.
   intros. dependent induction H.
-  - exists t0'. split.
-    + constructor.
+  - eexists. splits. apply rtn1_refl. assumption.
+  - destruct y. remember (pc_monotone_step V0 _ _ _ _ H H1).
+    edestruct IHclos_refl_trans_n1 as [t' [IHcomp IHabs]];
+      try reflexivity; try assumption.
+    destruct (correctness_step _ _ _ _ t' V0 H H1 IHabs) as [t_step [Hstep Habs_step]].
+    eexists. splits.
+    + econstructor. apply Hstep. apply IHcomp.
     + assumption.
-  - dependent destruction H.
-    + (* x := e*)
-      destruct (IHclos_refl_trans_n1 s <{x := e ; s'}> t0 t1) as [t' [comp IHV]];
-        try reflexivity; try assumption.
-      exists (t' :: (x, Aeval_t V0 t' e)). split.
-      * eapply Relation_Operators.rtn1_trans. apply CAsgn_step. assumption.
-      * unfold acc_subst_id, Aeval_t in *. simpl.
-        rewrite asgn_sound. rewrite <- IHV. reflexivity.
-    + (* s0 || s' *)
-      destruct (IHclos_refl_trans_n1 s <{ s0 || s''}> t0 t1) as [t' [comp IHV]];
-        try (apply (pc_monotone _ _ _ _ _ H));
-        try assumption; try reflexivity.
-      edestruct (par_step_correct _ _ _ _ t' V0 H H1 IHV) as [tc [stepL [_ IHV']]].
-      exists tc. split.
-      * eapply Relation_Operators.rtn1_trans. apply stepL. apply comp.
-      * apply IHV'.
-    + (* s' || s0 *)
-      destruct (IHclos_refl_trans_n1 s <{ s'' || s0}> t0 t1) as [t' [comp IHV]];
-        try (apply (pc_monotone _ _ _ _ _ H));
-        try assumption; try reflexivity.
-      edestruct (par_step_correct _ _ _ _ t' V0 H H1 IHV) as [tc [_ [stepR IHV']]].
-      exists tc. split.
-      * eapply Relation_Operators.rtn1_trans. apply stepR. apply comp.
-      * apply IHV'.
-    + (* if true*)
-      simpl in H1. apply andb_true_iff in H1. destruct H1.
-      destruct (IHclos_refl_trans_n1 s <{if b {s1}{s2} ; s0}> t0 t1) as [t' [comp IHV]];
-        try reflexivity; try assumption.
-      exists t'. splits.
-      * eapply Relation_Operators.rtn1_trans. apply CIfTrue_step;
-          unfold Bapply_t in H; rewrite <- comp_subB in H;
-          rewrite <- IHV in H; unfold Beval_t; apply H.
-        apply comp.
-      * unfold acc_subst_id. simpl. assumption.
-    + (* if false*)
-      simpl in H1. apply andb_true_iff in H1. destruct H1.
-      apply negb_true_iff in H.
-      destruct (IHclos_refl_trans_n1 s <{if b {s1}{s2} ; s0}> t0 t1) as [t' [comp IHV]];
-        try reflexivity; try assumption.
-      exists t'. splits.
-      * eapply Relation_Operators.rtn1_trans. apply CIfFalse_step;
-          unfold Bapply_t in H; rewrite <- comp_subB in H;
-          rewrite <- IHV in H; unfold Beval_t; apply H.
-        apply comp.
-      * unfold acc_subst_id. simpl. assumption.
-    (* skips *)
-    + destruct (IHclos_refl_trans_n1 s <{ skip || s' }> t0 t) as [t' [comp IHV]];
-        try reflexivity; try assumption.
-      exists t'. splits.
-      * eapply Relation_Operators.rtn1_trans. apply CSkipLeft_step. apply comp.
-      * assumption.
-    + destruct (IHclos_refl_trans_n1 s <{ s' || skip }> t0 t) as [t' [comp IHV]];
-        try reflexivity; try assumption.
-      exists t'. splits.
-      * eapply Relation_Operators.rtn1_trans. apply CSkipRight_step. apply comp.
-      * assumption.
 Qed.
 
 (** skipping "simple" completeness for now
